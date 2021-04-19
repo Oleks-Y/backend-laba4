9733c735727ba52e3f86c1f33c76ee13de888801
Oleks-Y <borith1111@gmail.com>
2021-04-19 23:12:31 +0300

    blyadskiy connector

--- connectors/MySqlConnector.py
+++ connectors/MySqlConnector.py
@@ -1,16 +1,42 @@
-import mysql 
+import pymysql
+from pymysql.cursors import DictCursor
 from Connector import Connector
+import configparser
 
-class MySqlConnector(Connector): 
-    def __init__(self, conn): 
-        super().__init__(conn)
-    def select(self, fields, table, query):
-        pass 
-    def insert(self, data, table):
-        pass 
-    def update(self, data, table, query):
-        pass 
-    def delete(self, table, query):
-        pass 
-    def createDatabase(self): 
+
+class MySqlConnector(Connector):
+    def __init__(self):
+        super().__init__()
+        config = configparser.ConfigParser()
+        config.read_file(open("conf.ini", "r"))
+        if not 'MySql' in config:
+            raise Exception("Please specify Postgres config")
+        dbConf = config["Postgres"]
+        self.conn = pymysql.connect(user='blyat',
+                                    password='12345678',
+                                    host='0.0.0.0',
+                                    db='univercity',
+                                    port=3307,
+                                    charset='utf8mb4',
+                                    cursorclass=DictCursor,
+                                    )
         pass
+
+    def select(self, fields, table, query):
+        pass
+
+    def insert(self, data, table):
+        pass
+
+    def update(self, data, table, query):
+        pass
+
+    def delete(self, table, query):
+        pass
+
+    def createDatabase(self):
+        pass
+
+
+a = MySqlConnector()
+print("Done")
